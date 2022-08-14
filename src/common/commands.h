c091732349366e743902b41f1b203eeb4f71e941
cahamo <83452365+cahamo@users.noreply.github.com>
2022-08-14 10:19:39 +0100

    Add header files for common use
    Required by central and IR control MCUs
    
    * commands.h - command related macros
    * debug.h - optional debug output over Serial - works iff DEBUG defined
    * i2c.h - i2C related macros

---
+++ src/common/commands.h
@@ -0,0 +1,109 @@
+/*
+ * commands.h
+ *
+ * Defines command related macros.
+ *
+ * These macros are common to all programs in the diorama project.
+ */
+
+#ifndef COMMAND_H
+#define COMMAND_H
+
+// Commands
+
+#define CMD_LIGHTING    1
+#define CMD_FEATURE     2
+#define CMD_AMBIENT     3
+#define CMD_PROGRAM     4
+#define CMD_RESET       5
+
+// Sub-commands
+
+#define SUBCMD_LIGHTING_LIGHT       1
+#define SUBCMD_LIGHTING_SECTION     2
+#define SUBCMD_LIGHTING_ALL         3
+
+#define SUBCMD_FEATURE_ACTIVATE     1
+#define SUBCMD_FEATURE_RESET        2
+
+#define SUBCMD_AMBIENT_SWITCH       1
+#define SUBCMD_AMBIENT_BRIGHTNESS   2
+
+#define SUBCMD_PROGRAM_RUN          1
+#define SUBCMD_PROGRAM_STOP         2
+
+#define SUBCMD_NULL                 0
+
+// Special parameter values
+
+#define CMDPARAM_LIGHTING_ALL_EVERYTHING  0x01
+#define CMDPARAM_LIGHTING_ALL_NORMAL      0x02
+#define CMDPARAM_LIGHTING_ALL_FLICKER     0x03
+
+#define CMDPARAM_AMBIENT_SWITCH_OFF       0x00
+#define CMDPARAM_AMBIENT_SWITCH_ON        0xFF
+
+#define CMDPARAM_AMBIENT_BRIGHTNESS_UP    0x01 // +1
+#define CMDPARAM_AMBIENT_BRIGHTNESS_DOWN  0xFF // -1
+
+#define CMDPARAM_NULL                     0x00
+
+// Command macros
+
+#define MakeCommand(mainCmd, subCmd) \
+    ((((mainCmd) & 0x0F) << 4) | ((subCmd) & 0x0F))
+#define ExtractMainCommand(cmd) \
+    ((cmd) >> 4)
+#define ExtractSubCommand(cmd) \
+    ((cmd) & 0x0F)
+#define MakeSectionLightParam(sectionId, lightId) \
+    ((((sectionId) & 0x0F) << 4) | ((lightId) & 0x0F))
+
+// Parameter IDs
+
+#define LIGHTING_STORE_SECION_ID        0
+#define LIGHTING_FORGE_SECTION_ID       1
+#define LIGHTING_OFFICE_SECTION_ID      2  // TBC: awaiting construction
+#define LIGHTING_WORKSHOP_SECTION_ID    3  // TBC: awaiting construction
+#define LIGHTING_LEANTO_SECTION_ID      4  // TBC: awaiting construction
+
+#define LIGHTING_FLICKER_LIGHT_1_ID     12
+#define LIGHTING_FLICKER_LIGHT_2_ID     13
+#define LIGHTING_FLICKER_LIGHT_BASE     LIGHTING_FLICKER_LIGHT_1_ID
+
+// Number of valid IDs for certain command types
+#define LIGHTING_SECTION_COUNT  5
+#define FEATURE_COUNT           3
+#define PROGRAM_COUNT           1
+
+// Number of lights in each lighting section
+#define LIGHTING_STORE_NORMAL_LIGHT_COUNT       2
+#define LIGHTING_STORE_FLICKER_LIGHT_COUNT      1
+#define LIGHTING_FORGE_NORMAL_LIGHT_COUNT       4
+#define LIGHTING_FORGE_FLICKER_LIGHT_COUNT      2 // Count TBC
+#define LIGHTING_OFFICE_NORMAL_LIGHT_COUNT      1 // TBC: awaiting construction
+#define LIGHTING_OFFICE_FLICKER_LIGHT_COUNT     1 // TBC: awaiting construction
+#define LIGHTING_WORKSHOP_NORMAL_LIGHT_COUNT    2 // TBC: awaiting construction
+#define LIGHTING_WORKSHOP_FLICKER_LIGHT_COUNT   1 // TBC: awaiting construction
+#define LIGHTING_LEANTO_NORMAL_LIGHT_COUNT      1 // TBC: awaiting construction
+#define LIGHTING_LEANTO_FLICKER_LIGHT_COUNT     0 // TBC: awaiting construction
+
+// Map of lighting sections to number of normal lights in section
+byte lightingNormalLightsInSection[LIGHTING_SECTION_COUNT] = {
+    LIGHTING_STORE_NORMAL_LIGHT_COUNT,
+    LIGHTING_FORGE_NORMAL_LIGHT_COUNT,
+    LIGHTING_OFFICE_NORMAL_LIGHT_COUNT,
+    LIGHTING_WORKSHOP_NORMAL_LIGHT_COUNT,
+    LIGHTING_LEANTO_NORMAL_LIGHT_COUNT
+};
+
+// Map of lighting sections to number of flicker lights in section
+byte lightingFlickerLightsInSection[LIGHTING_SECTION_COUNT] = {
+    LIGHTING_STORE_FLICKER_LIGHT_COUNT,
+    LIGHTING_FORGE_FLICKER_LIGHT_COUNT,
+    LIGHTING_OFFICE_FLICKER_LIGHT_COUNT,
+    LIGHTING_WORKSHOP_FLICKER_LIGHT_COUNT,
+    LIGHTING_LEANTO_FLICKER_LIGHT_COUNT
+};
+
+#endif
---
+++ src/common/i2c.h
@@ -0,0 +1,14 @@
+/*
+ * i2c.h
+ *
+ * Defines I2C related macros.
+ *
+ * These macros are common to all programs in the diorama project.
+ */
+
+#ifndef I2C_H
+#define I2C_H
+
+#define CENTRAL_CONTROL_I2C_ADDR 0x04
+
+#endif
